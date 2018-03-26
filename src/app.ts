812f5eb188d06080863ab03056a84493ca0db342
Barend Erasmus <developersworkspace@gmail.com>
2018-03-26 09:13:29 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,69 @@
-import { LeakyBucketAlgorithm } from './leaky-bucket-algorithm';
+import * as net from 'net';
+import { DistributedLockManager } from './distributed-lock-manager';
 
-const leakyBucketAlgorithm: LeakyBucketAlgorithm = new LeakyBucketAlgorithm(200, 10);
+function handleSocket(socket: net.Socket): void {
+    let socketBuffer: Buffer = null;
 
-console.log(leakyBucketAlgorithm.toString());
+    socket.on('data', (data: Buffer) => {
+        socketBuffer = socketBuffer ? Buffer.concat([socketBuffer, data]) : data;
 
-function setupTimeout() {
-    setTimeout(() => {
-        if (leakyBucketAlgorithm.addDrop()) {
-            console.log(`Added (${leakyBucketAlgorithm.getNumberOfDrops()})`);
-        } else {
-            console.log(`Overflow (${leakyBucketAlgorithm.getNumberOfDrops()})`);
+        let command: number[] = [];
+
+        for (const char of socketBuffer) {
+            if (!char) {
+                continue;
+            }
+
+            if (char === 10) {
+                handleCommand(command, socket).then(() => { });
+
+                command = [];
+                continue;
+            }
+
+            command.push(char);
         }
 
-        setupTimeout();
-    }, (500 * Math.random()) + 50);
+        socketBuffer = Buffer.from(command);
+    });
 }
 
-setupTimeout();
+async function handleCommand(command: number[], socket: net.Socket): Promise<void> {
+    command = command.filter((x) => x !== 10 && x !== 13);
+
+    const commandString: string = String.fromCharCode.apply(null, command);
+
+    const splittedCommandString: string[] = commandString.split(' ');
+
+    const action: string = splittedCommandString[0];
+
+    const parameters: string[] = splittedCommandString.slice(1);
+
+    const distributedLockManager: DistributedLockManager = DistributedLockManager.getInstance(parameters[0], 1000);
+
+    if (action === 'acquire') {
+        // acquire <name>
+        const result: boolean = distributedLockManager.acquire();
+
+        socket.write(result ? 'TRUE\r\n' : 'FALSE\r\n');
+    } else if (action === 'release') {
+        // release <name>
+
+        distributedLockManager.release();
+
+        socket.write('OK\r\n');
+    } else if (action === 'waitAcquire') {
+        // waitAcquire <name>
+        const result: boolean = await distributedLockManager.waitAcquire();
+
+        socket.write(result ? 'TRUE\r\n' : 'FALSE\r\n');
+    } else {
+        socket.write('Invalid Command\r\n');
+    }
+}
+
+net.createServer(handleSocket).listen(5000);
+
+const port: number = 5000;
+
+console.log(`listening on ${port}`);
---
+++ src/distributed-lock-manager.ts
@@ -0,0 +1,85 @@
+import { State } from './enums/state';
+
+export class DistributedLockManager {
+
+    private static instances: {} = {};
+
+    private maxWaitForAcquire: number = 1000;
+
+    private state: State = State.Unlocked;
+
+    private timestamp: Date = null;
+
+    constructor(
+        private name: string,
+        private timeoutInMilliseconds: number,
+    ) {
+        this.maxWaitForAcquire = timeoutInMilliseconds * 3;
+    }
+
+    public acquire(): boolean {
+        const currentTimestamp: Date = new Date();
+
+        if (this.state === State.Unlocked) {
+            this.state = State.Locked;
+            this.timestamp = currentTimestamp;
+
+            return true;
+        }
+
+        if (this.timestamp.getTime() + this.timeoutInMilliseconds < currentTimestamp.getTime()) {
+            this.state = State.Locked;
+            this.timestamp = currentTimestamp;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public static getInstance(name: string, timeoutInMilliseconds: number): DistributedLockManager {
+        let instance: DistributedLockManager = DistributedLockManager.instances[name];
+
+        if (instance) {
+            return instance;
+        }
+
+        instance = new DistributedLockManager(name, timeoutInMilliseconds);
+
+        DistributedLockManager.instances[name] = instance;
+
+        return instance;
+    }
+
+    public release(): void {
+        if (this.state === State.Locked) {
+            this.state = State.Unlocked;
+            this.timestamp = null;
+        }
+    }
+
+    public async waitAcquire(): Promise<boolean> {
+        const delayMilliseconds: number = 100;
+
+        for (let count: number = 0; count < this.maxWaitForAcquire / delayMilliseconds; count ++) {
+            const result: boolean = await this.acquire();
+
+            if (result) {
+                return true;
+            }
+
+            await this.delay(delayMilliseconds);
+        }
+
+        return false;
+    }
+
+    private delay(milliseconds: number): Promise<void> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve();
+            }, milliseconds);
+        });
+    }
+
+}
